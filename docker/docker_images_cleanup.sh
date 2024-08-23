2e953e50fe8a1587044b9b7e519d5e638a2170c6
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2024-08-23 10:33:51 +0200

    Create docker_images_cleanup.sh

---
+++ docker/docker_images_cleanup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+# List dangling Docker images
+list_dangling_images() {
+    docker images --quiet --filter=dangling=true
+}
+
+# Remove dangling images
+remove_dangling_images() {
+    local image_ids="$1"
+    if [ -n "$image_ids" ]; then
+        echo "Removing dangling images..."
+        echo "$image_ids" | xargs --no-run-if-empty docker rmi
+    else
+        echo "No dangling images found."
+    fi
+}
+
+# Main script
+main() {
+    local dangling_images=$(list_dangling_images)
+    remove_dangling_images "$dangling_images"
+}
